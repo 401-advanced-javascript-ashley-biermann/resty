51e8107ea226690a943ef7cb1922b7b493281c8f
ashleybiermann <ashleybiermann@gmail.com>
2020-07-14 00:38:49 -0700

    reasonably passing css

--- src/components/form/form.js
+++ src/components/form/form.js
@@ -36,35 +36,32 @@ class Form extends React.Component {
           <input type="text" value={this.state.value} onChange={this.handleChange} placeholder="URL"></input>
 
           <button type="submit" value="Submit"><span>Search the API</span></button>
-
-          <section id="methods">
-            <button onClick={() => this.setState({
-              method: 'GET',
-            })} type="radio" value="get"><span>GET</span></button>
-
-            <button onClick={() => this.setState({
-              method: 'POST',
-            })} type="radio" value="post"><span>POST</span></button>
-
-            <button onClick={() => this.setState({
-              method: 'PUT',
-            })} type="radio" value="put"><span>PUT</span></button>
-
-            <button onClick={() => this.setState({
-              method: 'PATCH',
-            })} type="radio" value="patch"><span>PATCH</span></button>
-
-            <button onClick={() => this.setState({
-              method: 'DELETE',
-            })} type="radio" value="delete"><span>DELETE</span></button>
-          </section>
         </form>
 
+        <section id="methods">
+          <button onClick={() => this.setState({
+            method: 'get',
+          })} type="radio" value="get"><span>GET</span></button>
+
+          <button onClick={() => this.setState({
+            method: 'post',
+          })} type="radio" value="post"><span>POST</span></button>
+
+          <button onClick={() => this.setState({
+            method: 'put',
+          })} type="radio" value="put"><span>PUT</span></button>
+
+          <button onClick={() => this.setState({
+            method: 'patch',
+          })} type="radio" value="patch"><span>PATCH</span></button>
+
+          <button onClick={() => this.setState({
+            method: 'delete',
+          })} type="radio" value="delete"><span>DELETE</span></button>
+        </section>
+
         <section id="apiRequest">
-          <p>
-            {this.state.method} <br></br>
-            {this.state.value}
-          </p>
+          <p>{this.state.method} {this.state.value}</p>
         </section>
       </div>
     );
