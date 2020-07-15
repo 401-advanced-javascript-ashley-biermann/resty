54dcda4685ceb88d62800f1cc0f2ffb0884ddb50
ashleybiermann <runner@fv-az64.fmoh14e1fqrenbrdmbhkwp5hwh.bx.internal.cloudapp.net>
2020-07-15 22:40:17 +0000

    Updates

---
+++ precache-manifest.63304b015f41e8ca75aa7a109b69fb51.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eedda0353d8324235229efe8cf7edb0a",
+    "url": "/resty/index.html"
+  },
+  {
+    "revision": "1a5eaa5164026ef062f6",
+    "url": "/resty/static/css/main.86465bc5.chunk.css"
+  },
+  {
+    "revision": "210cc5293356f468c018",
+    "url": "/resty/static/js/2.348daa53.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/resty/static/js/2.348daa53.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1a5eaa5164026ef062f6",
+    "url": "/resty/static/js/main.69a4e87d.chunk.js"
+  },
+  {
+    "revision": "a82df10f567bbeaa6ec2",
+    "url": "/resty/static/js/runtime-main.ea9bec24.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/resty/precache-manifest.3b51614f647a14a0857307505f37fc0d.js"
+  "/resty/precache-manifest.63304b015f41e8ca75aa7a109b69fb51.js"
 );
 
 self.addEventListener('message', (event) => {
